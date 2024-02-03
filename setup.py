fddc42d54ead5dec47b04cb3de4a2e1d94339245
khollek42 <41762992+khollek42@users.noreply.github.com>
2024-02-03 01:57:17 -0600

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='gui.py',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='kyles',
+    author_email='',
+    description=''
+)
